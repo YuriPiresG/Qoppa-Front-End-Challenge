d22b9cf023e43ca319396586bfe4c4f51cad5ef1
Yuri Pires <yuripiresbr@gmail.com>
2023-10-29 22:21:02 -0300

    first commit

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,45 +1,70 @@
-import express from 'express';
-import chalk from 'chalk';
-import { Request, Response } from 'express';
-import figlet from 'figlet';
+import express from "express";
+import chalk from "chalk";
+import { Request, Response } from "express";
+import figlet from "figlet";
+import cors from "cors";
 
-await figlet('QoppaTech', (err, data) => {
-    if(err) {
-        console.log(chalk.yellow("CANNOT LOAD FIGLET ART"));
-    }
+await figlet("QoppaTech", (err, data) => {
+  if (err) {
+    console.log(chalk.yellow("CANNOT LOAD FIGLET ART"));
+  }
 
-    console.log(chalk.blue(data));
-})
-console.log(chalk.blue("Bem vindo ao CLI da API do teste de front-end da QoppaTech."));
-console.log(chalk.blue("Confira as informações em: https://github.com/QoppaTech/Qoppa-Front-End-Challenge/tree/main/api"));
-console.log("----------------------------------------------------------------")
-
+  console.log(chalk.blue(data));
+});
+console.log(
+  chalk.blue("Bem vindo ao CLI da API do teste de front-end da QoppaTech.")
+);
+console.log(
+  chalk.blue(
+    "Confira as informações em: https://github.com/QoppaTech/Qoppa-Front-End-Challenge/tree/main/api"
+  )
+);
+console.log("----------------------------------------------------------------");
 
 const app = express();
 
-app.use( express.json() );
+// Add CORS middleware to allow requests from all origins. You can configure it more restrictively.
+app.use(cors());
 
-app
-.get("/", (req: Request, res: Response) => res.status(200).send("<h1>API DESAFIO: QoppaTech </h1>"))
-.post("/singUp", (req: Request, res: Response) => {
-    console.log(chalk.yellow("⏺ Requisição do tipo POST recebida na rota correta!"));
+app.use(express.json());
 
-    const bd = req.body;
+app.get("/", (req: Request, res: Response) =>
+  res.status(200).send("<h1>API DESAFIO: QoppaTech </h1")
+);
 
-    if(bd.username, bd.email, bd.password) {
-        console.log(chalk.green("✅ Reconhecido! Parabéns!"))
-        return res.status(200).send();
-    } else {
-        console.log(chalk.red("⛔️ Dados enviados incorretos ou não recebidos, confira os dados que devem ser recebidos em: https://github.com/QoppaTech/Qoppa-Front-End-Challenge/tree/main/api"));
-        return res.status(401).send(); 
-    }
-})
-.post("*", (req: Request, res: Response) => {
-    console.log(chalk.red("❌ Requisição do tipo POST recebida na rota incorreta!"));
-    return res.status(404).send();
-})
-.get("*", (req: Request, res: Response) => {
-    return res.status(404).send("<h1>API DESAFIO: 404 -> PÁGINA NÃO ENCONTRADA -> quoppaTech </h1>")
-})
+app.post("/singUp", (req: Request, res: Response) => {
+  console.log(
+    chalk.yellow("⏺ Requisição do tipo POST recebida na rota correta!")
+  );
 
-app.listen(3000, () => console.log(chalk.green(`Qoppa_Desafio: Rodando na porta 3000`)));
+  const bd = req.body;
+
+  if (bd.username && bd.email && bd.password) {
+    console.log(chalk.green("✅ Reconhecido! Parabéns!"));
+    return res.status(200).send();
+  } else {
+    console.log(
+      chalk.red(
+        "⛔️ Dados enviados incorretos ou não recebidos, confira os dados que devem ser recebidos in: https://github.com/QoppaTech/Qoppa-Front-End-Challenge/tree/main/api"
+      )
+    );
+    return res.status(401).send();
+  }
+});
+
+app.post("*", (req: Request, res: Response) => {
+  console.log(
+    chalk.red("❌ Requisição do tipo POST recebida na rota incorreta!")
+  );
+  return res.status(404).send();
+});
+
+app.get("*", (req: Request, res: Response) => {
+  return res
+    .status(404)
+    .send("<h1>API DESAFIO: 404 -> PÁGINA NÃO ENCONTRADA -> QoppaTech </h1>");
+});
+
+app.listen(3000, () =>
+  console.log(chalk.green(`Qoppa_Desafio: Rodando na porta 3000`))
+);
